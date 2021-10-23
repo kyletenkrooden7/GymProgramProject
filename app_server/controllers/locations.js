8fbe911a76d5446c692f1e7637bb7585d5a602c4
Kyle Tenkrooden <kyle.tenkrooden@students.ittralee.ie>
2021-10-23 13:30:59 +0100

    first commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -16,8 +16,68 @@ const register = function(req, res){
 
 /* GET 'Add generalHealth' page */
 const general = function(req, res){
-  res.render('GeneralHealth', { title: 'GeneralHealth' });
+  res.render('GeneralHealth', { title: 'GeneralHealth',
+  exerciseHeaders: [{
+    nameCol: 'Exercise',
+    warmUpCol: 'Warm-Up Sets',
+    setsCol: 'Sets',
+    repsCol: 'Reps',
+    repMaxCol: '%1RM',
+    restCol: 'Rest',
+    demoCol: 'Demo'
+  }],
+  exercises: [{
+        name: 'Barbell Bench Press',
+        warmUpSets: '3',
+        sets: '3',
+        reps: '12',
+        repMax: '60%',
+        rest: '2-3 min',
+        demo: 'https://youtu.be/rT7DgCr-3pg'
+    }, {
+        name: 'Thrusters',
+        warmUpSets: '3',
+        sets: '3',
+        reps: '10',
+        repMax: '60%',
+        rest: '2-3 min',
+        demo: 'https://youtu.be/Zvt5-mugUco'
+    }, {
+        name: 'Jumping Squats',
+        warmUpSets: '3',
+        sets: '3',
+        reps: '12',
+        repMax: '60%',
+        rest: '2-3 min',
+        demo: 'https://youtu.be/A-cFYWvaHr0'
+    }, {
+        name: 'Pull Ups',
+        warmUpSets: '3',
+        sets: '3',
+        reps: '1',
+        repMax: '60%',
+        rest: '2-3 min',
+        demo: 'https://youtu.be/A-cFYWvaHr0'
+    }, {        
+        name: 'Mountain Climbers',
+        warmUpSets: '3',
+        sets: '3',
+        reps: '10',
+        repMax: '70%',
+        rest: '1-2 min',
+        demo: 'https://youtu.be/ZhiCSdOVJp0'
+    }, {        
+        name: 'Jumping Jacks',
+        warmUpSets: '3',
+        sets: '3',
+        reps: '12',
+        repMax: '60%',
+        rest: '1-2 min',
+        demo: 'https://youtu.be/1b98WrRrmUs'
+    }]
+  });
 };
+
 
 /* GET 'Add BuildMuscle' page */
 const buildMuscle = function(req, res){
