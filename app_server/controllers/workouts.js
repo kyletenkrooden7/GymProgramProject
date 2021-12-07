703e228ed1e3b5ff53cfd1dd39c58df1d16bbec2
kyletenkrooden7 <k.tenkrooden7@gmail.com>
2021-12-07 23:47:14 +0000

    heroku commit

--- app_server/controllers/workouts.js
+++ app_server/controllers/workouts.js
@@ -3,7 +3,7 @@ const apiOptions = {
 server : 'http://localhost:3000' 
 }; 
 if (process.env.NODE_ENV === 'production') { 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 //apiOptions.server = 'https://intense-ocean-11035.herokuapp.com/'; 
 }
 
